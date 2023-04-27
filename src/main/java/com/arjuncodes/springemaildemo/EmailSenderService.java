fd5771dce304551dba7e25eff170187533ec4562
Nizar <nizar.al.nasraoui@gmail.com>
2023-04-28 01:52:05 +0200

    init project

---
+++ src/main/java/com/arjuncodes/springemaildemo/EmailSenderService.java
@@ -0,0 +1,41 @@
+package com.arjuncodes.springemaildemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@Service
+public class EmailSenderService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleEmail(String toEmail,
+                                String subject,
+                                String body
+    ){
+
+        try{
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(toEmail);
+            helper.setFrom("nizarnasraouif@gmail.com");
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            mailSender.send(message);
+        }
+        catch (MessagingException messagingException){
+            messagingException.printStackTrace();
+        }
+
+
+    }
+
+    }
+
